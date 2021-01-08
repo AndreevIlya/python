1a07b2d061c25901112f69846a113736a74cfb94
Ilya Andreev <ilya.andreev@keemoji.com>
2021-01-08 21:50:05 +0300

    Lesson-3: Amend previous commit to proceed a sentence

--- task6.py
+++ task6.py
@@ -1,6 +1,6 @@
-print(
-    (lambda string: string.title())
-    (
-        input("Enter a word: ")
-    )
-)
+# PyCharm recommends not to assign lambda but to use def O_O
+capitalize_word = lambda string: string.title()
+
+string = input("Enter a sentence: ")
+for word in string.split(" "):
+    print(f"{capitalize_word(word)} ", end="")
